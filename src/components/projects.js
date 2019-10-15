7fac613737a9525117bdcd684b418c5c973e292c
neaj mahmud <neajmahmud230@gmail.com>
2019-10-15 16:21:07 -0400

    changed project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,134 +1,76 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import React, { Component } from 'react'
 import "./projects.css"
-
-
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: 0 };
-  }
-
-  toggleCategories() {
-
-    if(this.state.activeTab === 0){
-      return(
-        <div className="projects-grid">
-          {/* Project 1 */}
-          <Card  shadow={5} style={{minWidth: '450', marginTop: ' 30px ',borderRadius: "10px"}}>
-            <CardTitle style={{ height: '176px', background: 'url(https://rovinox3.s3.amazonaws.com/bucketFolder/1558990399545-lg.png) center / cover'}} >
-            </CardTitle>
-            <CardText>
-               <h2>Rovinox</h2>
-              <h5>Developed a user based E-commerce site to buy and sell products.</h5>
-              <ul>
-                <li>Bcript.js to secure user login credentials and PostgreSQL to store the data.</li>
-                <li>Aws s3 to store uploaded files. </li>
-                <li>React and redux in the front-end to work with data. </li>
-                <li>Stripe API to accept payment from user. </li>
-                <li>Implemented various Validation Controls for form validation and implemented custom validation controls using Object Oriented JavaScript. </li>
-                <li>Responsible for React UI and architecture.</li>
-
-              </ul>
-              
-            </CardText>
-            <CardActions border className="Card-Actions">
-            
-             <a href="https://github.com/rovinox/offer-up" rel="noopener noreferrer" target="_blank">
-             <button className="btn-project" >GitHub</button>
-             </a>
-             
-             <a href="http://rovinox.xyz" rel="noopener noreferrer" target="_blank">
-             <button className="btn-project">Website</button>
-             </a> 
-             
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              
-            </CardMenu>
-          </Card>
-
-          <Card  shadow={5} style={{minWidth: '450', marginTop: ' 30px',borderRadius: "10px"}}>
-            <CardTitle style={{ height: '176px', background: 'url(https://rovinox3.s3.amazonaws.com/bucketFolder/1560188679430-lg.png) center / cover'}} >
-            </CardTitle>
-            <CardText>
-               <h2>TurboLog</h2>
-              <h5>How to keep track of your car maintenance history</h5>
-              <h6>react, material-ui, redux, node, express, bcrypt, react hooks</h6>
-              <ul>
-                <li>group project that was complated in two weeks</li>
-                <li>Worked heavily with the google map places and geocoding API</li>
-                <li>Led group in several breakout sessions regarding the best approach to challenges throughout the project </li>
-                <li>Entire project created in react hooks</li>
-                <li>Was built mobile-first as to more strongly appeal to the main demographic </li>
-                
-
-              </ul>
-              
-            </CardText>
-            <CardActions border className="Card-Actions">
-            
-             <a href="https://github.com/turbolog/turbo-log" rel="noopener noreferrer" target="_blank">
-             <button className="btn-project" >GitHub</button>
-             </a>
-             
-             <a href="http://turbolog.xyz/#/" rel="noopener noreferrer" target="_blank">
-             <button className="btn-project">Website</button>
-             </a> 
-             
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              
-            </CardMenu>
-          </Card>
-
-         
-
-          
-          
-        </div>
-
-
-      )
-    } else if(this.state.activeTab === 1) {
-      return (
-        <div><h1>This is Angular</h1></div>
-      )
-    } else if(this.state.activeTab === 2) {
-      return (
-        <div><h1>This is VueJS</h1></div>
-      )
-    } else if(this.state.activeTab === 3) {
-      return (
-        <div><h1>This is MongoDB</h1></div>
-      )
-    }
-
-  }
-
-
-
+import gif from "../components/gif/myGif.gif"
+import {Card, CardTitle,CardText,CardActions,Button } from "react-mdl"
+export default class projects extends Component {
   render() {
-    return(
-      <div>
-        {/* <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>React</Tab>
-          <Tab>Angular</Tab>
-          <Tab>VueJS</Tab>
-          <Tab>MongoDB</Tab>
-        </Tabs> */}
-
-
-          <Grid>
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
-            </Cell>
-          </Grid>
-
-
+    return (
+     
+        
+    <div>
+      <div className="background">
+      <div className="water"></div>
       </div>
+      <div style={{display:"flex", justifyContent:"space-around", flexWrap:"wrap", }}>
+
+      <Card shadow={0} style={{width: '400px', height: '320px', marginTop:"20px", borderRadius:"15px"}}>
+        <CardTitle expand style={{color: 'red', background: `url(${gif}) center / cover`,}}></CardTitle>
+        <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Aenan convallis.
+        </CardText>
+        <CardActions border>
+        <Button colored>Github</Button>
+        <Button colored>Website</Button>
+        </CardActions>
+      </Card>
+      <Card shadow={0} style={{width: '400px', height: '320px', marginTop:"20px", borderRadius:"15px"}}>
+        <CardTitle expand style={{color: '#fff', background: `url(${gif}) center / cover`,}}></CardTitle>
+        <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Aenan convallis.
+        </CardText>
+        <CardActions border>
+        <Button colored>Github</Button>
+        <Button colored>Website</Button>
+        </CardActions>
+      </Card>
+      <Card shadow={0} style={{width: '400px', height: '320px', marginTop:"20px", borderRadius:"15px"}}>
+        <CardTitle expand style={{color: '#fff', background: `url(${gif}) center / cover`,}}></CardTitle>
+        <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Aenan convallis.
+        </CardText>
+        <CardActions border>
+        <Button colored>Github</Button>
+        <Button colored>Website</Button>
+        </CardActions>
+      </Card>
+      <Card shadow={0} style={{width: '400px', height: '320px', marginTop:"20px", borderRadius:"15px"}}>
+        <CardTitle expand style={{color: '#fff', background: `url(${gif}) center / cover`,}}></CardTitle>
+        <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Aenan convallis.
+        </CardText>
+        <CardActions border>
+        <Button colored>Github</Button>
+        <Button colored>Website</Button>
+        </CardActions>
+      </Card>
+      </div>
+      <svg>
+      <filter id="turbulence" x="0" y="0" width="1400" height="100%">
+      <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
+      <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+      <animate xlinkHref="#sea-filter" attributeName="baseFrequency" dur="60s" 
+      keyTimes="0;0.5;1" values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite"/>
+      </filter>
+      </svg>
+  </div>
+
+
+  
+     
     )
   }
 }
-
-export default Projects;
