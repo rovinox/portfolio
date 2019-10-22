8a1c5466ca534465abec97f33686db0f890c2f77
neaj mahmud <neajmahmud230@gmail.com>
2019-10-21 23:27:19 -0400

    added all 4 projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import "./projects.css"
 import turbolog from "../components/gif/turbolog.gif"
+import rovinox from "../components/gif/rovinox.gif"
+import portfolio from "../components/gif/portfolio.gif"
+import amacon from "../components/gif/amacon.gif"
 import {Card, CardTitle,CardText,CardActions,Button } from "react-mdl"
 export default class projects extends Component {
   render() {
@@ -13,13 +16,27 @@ export default class projects extends Component {
       </div>
       <div style={{display:"flex", justifyContent:"space-around", flexWrap:"wrap", }}>
 
-      <Card shadow={0} style={{width: '500px', height: '500px', marginTop:"20px", borderRadius:"15px", textAlign:"center"}}>
+      <Card shadow={0} style={{width: '400px', height: '450px', marginTop:"20px", borderRadius:"15px", textAlign:"center"}}>
+        <CardTitle expand style={{color: 'red', background: `url(${amacon}) center / cover`,}}></CardTitle>
+        <CardText >
+            <h4>Amacon</h4>
+            <b>MERN Stack, Stripe API, Cloudinary API, Next.js, JWT, Semantic UI React</b>
+            <br/>
+            An e-commerce App with Full CRUD Functionality. Server-side Rendering for speed. Attractive + Responsive App Interface.
+       
+        </CardText>
+        <CardActions border>
+        <Button colored><a href="https://amacon.now.sh/" target="_blank">Website</a></Button>
+        <Button colored><a href="https://github.com/rovinox/amacon.git" target="_blank">github</a></Button>
+        </CardActions>
+      </Card>
+      <Card shadow={0} style={{width: '400px', height: '450px', marginTop:"20px", borderRadius:"15px", textAlign:"center"}}>
         <CardTitle expand style={{color: 'red', background: `url(${turbolog}) center / cover`,}}></CardTitle>
         <CardText >
             <h4>Turbolog</h4>
-            <b>React, Material-UI, Redux, Node, Express, Bcrypt, google maps API</b>
+            <b>React, Material-UI, Redux, Node, Express, Bcrypt, google maps API, PostgreSQL</b>
             <br/>
-                A app to keeps track of car maintenance history. Also you can find near by car mechanic shop and a forum section to ask any DYI qustion.
+                A app to keeps track of car maintenance history. Also you can find near by car mechanic shop and a forum section to ask any DYI qustions.
        
         </CardText>
         <CardActions border>
@@ -27,59 +44,35 @@ export default class projects extends Component {
         <Button colored><a href="https://github.com/turbolog/turbo-log" target="_blank">github</a></Button>
         </CardActions>
       </Card>
-      <Card shadow={0} style={{width: '400px', height: '400px', marginTop:"20px", borderRadius:"15px"}}>
-        <CardTitle expand style={{color: '#fff', background: `url(${turbolog}) center / cover`,}}></CardTitle>
-        <CardText>
-          
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis.
-        </CardText>
-        <CardActions border>
-        <Button colored>Github</Button>
-        <Button colored>Website</Button>
-        </CardActions>
-      </Card>
-      <Card shadow={0} style={{width: '400px', height: '400px', marginTop:"20px", borderRadius:"15px"}}>
-        <CardTitle expand style={{color: '#fff', background: `url(${turbolog}) center / cover`,}}></CardTitle>
-        <CardText>
-          
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis.
-        </CardText>
-        <CardActions border>
+      <Card shadow={0} style={{width: '400px', height: '450px', marginTop:"20px", borderRadius:"15px", textAlign:"center"}}>
+        <CardTitle expand style={{color: 'red', background: `url(${rovinox}) center / cover`,}}></CardTitle>
+        <CardText >
+            <h4>Rovinox</h4>
+            <b>Styled-components v4 Dynamic CSS-in-JS, React Context API</b>
+            <br/>
+            Build a Complete CryptoCurrency Financial Reporting App. Date Manipulation and Graphing Historical Price. 
        
-        </CardActions>
-      </Card>
-      <Card shadow={0} style={{width: '500px', height: '500px', marginTop:"20px", borderRadius:"15px"}}>
-        <CardTitle expand style={{color: '#fff', background: `url(${turbolog}) center / cover`,}}></CardTitle>
-        <CardText>
-          
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </CardText>
         <CardActions border>
-        <Button colored>Github</Button>
-        <Button colored>Website</Button>
+        <Button colored><a href="http://rovinox.xyz/" target="_blank">Website</a></Button>
+        <Button colored><a href="https://github.com/rovinox/rovinox.git" target="_blank">github</a></Button>
         </CardActions>
       </Card>
+      <Card shadow={0} style={{width: '400px', height: '450px', marginTop:"20px", borderRadius:"15px", textAlign:"center"}}>
+        <CardTitle expand style={{color: 'red', background: `url(${portfolio}) center / cover`,}}></CardTitle>
+        <CardText >
+            <h4>Portfolio</h4>
+            <b>React MDL material design, React Router v4, Nodemailer</b>
+            <br/>
+            I have build my own portfolio from the scratch instead of getting a template. Now i have the perfect portfolio i wanted. 
+       
+        </CardText>
+        <CardActions border>
+        <Button colored><a href="https://neajmahmud.dev/" target="_blank">Website</a></Button>
+        <Button colored><a href="https://github.com/rovinox/portfolio.git" target="_blank">github</a></Button>
+        </CardActions>
+      </Card>
+      
       </div>
       <svg>
       <filter id="turbulence" x="0" y="0" width="1400" height="100%">
