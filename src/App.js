899b4e24d5f9d14dad2a9c40e984fc838835de30
neaj mahmud <neajmahmud230@gmail.com>
2020-06-13 16:44:02 -0400

    working on bio

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link, withRouter} from 'react-router-dom';
 
+
 class App extends Component {
   render() {
    
     return (
+
+    
       <div className="demo-big-content">
     <Layout>
       {this.props.location.pathname === "/" || this.props.location.pathname === "/contact" ? <Header style={{background:"transparent"}} title={<Link style={{textDecoration: 'none', color: 'red',fontSize:"50px",fontFamily: 'Alfa Slab One'}} to="/">N M</Link>} scroll>
